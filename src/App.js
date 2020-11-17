56532f66d4ab8feac8c7cabf50ce71e992f5b343
piotr.smuga <smuga.piotr@hotmail.com>
2020-11-17 19:38:10 +0100

    Add Leaflet map and display current position of IIS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents, useMap } from 'react-leaflet';
+import { useEffect, useState } from 'react';
+
+function LocationMarker() {
+    const [position, setPosition] = useState(null);
+    const index = 1;
+    const map = useMapEvents({
+        click() {
+            map.locate();
+        },
+        locationfound(e) {
+            setPosition(e.latlng);
+            // map.flyTo(e.latlng, map.getZoom());
+        },
+    });
+
+    return position === null ? null : (
+        <Marker marker_index={index} key={index} position={position}>
+            <Popup>You are here</Popup>
+        </Marker>
+    );
+}
+
+function ISSMarker() {
+    const [position, setPosition] = useState(null);
+    const index = 2;
+    const map = useMap();
+    useEffect(() => {
+        async function getISS() {
+            const response = await fetch('https://api.wheretheiss.at/v1/satellites/25544');
+            const data = await response.json();
+            const { latitude, longitude } = data;
+            setPosition([latitude, longitude]);
+            map.flyTo([latitude, longitude], map.getZoom());
+        }
+        getISS();
+    }, [map]);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            async function getISS() {
+                const response = await fetch('https://api.wheretheiss.at/v1/satellites/25544');
+                const data = await response.json();
+                const { latitude, longitude } = data;
+                setPosition([latitude, longitude]);
+            }
+            getISS();
+        }, 5000);
+        return () => clearInterval(interval);
+    }, [map]);
+
+    return position === null ? null : (
+        <Marker marker_index={index} key={index} position={position}>
+            <Popup>Current ISS position</Popup>
+        </Marker>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [position] = useState([0, 0]);
+
+    return (
+        <div className='App'>
+            <MapContainer id='mapid' center={position} zoom={3} scrollWheelZoom={true}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                />
+                <ISSMarker></ISSMarker>
+                <LocationMarker></LocationMarker>
+            </MapContainer>
+        </div>
+    );
 }
 
 export default App;
